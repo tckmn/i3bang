9f76012b4ce42d44920dea02fc861741d0cf565b
KeyboardFire <andy@keyboardfire.com>
2015-06-09 07:08:15 -0500

    moved everything into a huge function, unit tests!

--- i3bang.rb
+++ i3bang.rb
@@ -1,170 +1,180 @@
 #!/usr/bin/ruby
 
-fname = ARGV[0] || '_config'
-INFILE = File.expand_path "~/.i3/#{fname}"
-OUTFILE = File.expand_path '~/.i3/config'
+def i3bang config, header = ''
 
-config = File.read INFILE
+    # kill comments; bangs in them interfere
+    # also annoying trailing whitespace
+    nobracket = config.include? '#!nobracket'
+    config.gsub! /\s*#.*\n/, "\n"
+    config.gsub! /\s+$/, ''
 
-# kill comments; bangs in them interfere
-# also annoying trailing whitespace
-nobracket = config.include? '#!nobracket'
-config.gsub! /\s*#.*\n/, "\n"
-config.gsub! /\s+$/, ''
+    # line continuations
+    config.gsub! /\\\n\s*/, ''
+    config += "\n"  # add back trailing newline
 
-# line continuations
-config.gsub! /\\\n\s*/, ''
-config += "\n"  # add back trailing newline
+    # add notice
+    # (feel free to remove/edit this; I don't mind)
+    config = header + config
 
-# add notice
-# (feel free to remove/edit this; I don't mind)
-config = "# Generated via i3bang (https://github.com/KeyboardFire/i3bang).
-# Original file: #{fname}\n" + config
-
-# change shorthand format to expanded, regular format if specified
-# UGLY HACK WARNING: !!<...!...> can interfere, so we're going to take
-# all existing ![@!]?<...>'s out and put them back in later.
-# BUT, expansions might contain !... and !!...s as well, so we have to rerun
-# this upon every expansion. Therefore, this is stuck inside a method.
-def expand_nobracket config
-    placeholder = '__PLCHLD__'
-    ph_arr = []
-    n = -1
-    config.gsub!(/![@!]?<[^>]*>/) {|m|
-        n += 1
-        ph_arr.push m
-        placeholder + "<#{n}>"
-    }
-    # now the actual substitutions
-    config.gsub! /^\s*(![@!]?)([^<@!\n][^!\n]*)$/, '\1<\2>'
-    config.gsub! /(![@!]?)([^<@!\s]\S*)/, '\1<\2>'
-    # replace the placeholders
-    config.gsub!(/#{placeholder}<(\d+)>/) { ph_arr[$1.to_i] }
-end
-expand_nobracket config if nobracket
-
-# first handle !@<...> sections
-i3bang_sections = Hash.new
-config.gsub!(/!@<([^>]*)>/) {
-    if $1.include? "\n"
-        name, data = $1.split "\n", 2
-        noecho = false
-        if name[0] == '*'
-            noecho = true
-            name = name[1..-1]
-        end
-        i3bang_sections[name] = data
-        noecho ? '' : data
-    else
-        i3bang_sections[$1]
+    # change shorthand format to expanded, regular format if specified
+    # UGLY HACK WARNING: !!<...!...> can interfere, so we're going to take
+    # all existing ![@!]?<...>'s out and put them back in later.
+    # BUT, expansions might contain !... and !!...s as well, so we have to rerun
+    # this upon every expansion. Therefore, this is stuck inside a method.
+    def expand_nobracket config
+        placeholder = '__PLCHLD__'
+        ph_arr = []
+        n = -1
+        config.gsub!(/![@!]?<[^>]*>/) {|m|
+            n += 1
+            ph_arr.push m
+            placeholder + "<#{n}>"
+        }
+        # now the actual substitutions
+        config.gsub! /^\s*(![@!]?)([^<@!\n][^!\n]*)$/, '\1<\2>'
+        config.gsub! /(![@!]?)([^<@!\s]\S*)/, '\1<\2>'
+        # replace the placeholders
+        config.gsub!(/#{placeholder}<(\d+)>/) { ph_arr[$1.to_i] }
     end
-}
-
-# then expand !!<...> into separate lines
-exrgx = /!!<([^>]*)>/
-while config =~ exrgx
-    config.sub!(/^.*#{exrgx}.*$/) {|line|
-        expansions = line.scan(exrgx).map{|expansion|
-            group, values = expansion[0].split('!', 2)
-            if values == nil
-                values = group
-                group = "__default_group"
-            end
-            [group, values.gsub(/(\d+)\.\.(\d+)/) {
-                [*$1.to_i..$2.to_i] * ?,
-            }.split(?,, -1)]
-        }
-        group = expansions[0][0]
-        # equalize length of values for same groups
-        maxlen = expansions.select{|g, _| g == group}.map(&:last).map(&:size).max
-        expansions.map! {|g, values|
-            g == group ?
-                [g, (values * (maxlen * 1.0 / values.length).ceil)[0...maxlen]] :
-                [g, values]
-        }
-        Array.new(expansions[0][1].length) { line.clone }.map {|l|
-            idx = -1
-            l.gsub(exrgx) {|m|
-                idx += 1
-                expansions[idx][0] == group ? expansions[idx][1].shift : m
-            }
-        }.join "\n"
-    }
     expand_nobracket config if nobracket
+
+    # first handle !@<...> sections
+    i3bang_sections = Hash.new
+    config.gsub!(/!@<([^>]*)>/) {
+        if $1.include? "\n"
+            name, data = $1.split "\n", 2
+            noecho = false
+            if name[0] == '*'
+                noecho = true
+                name = name[1..-1]
+            end
+            i3bang_sections[name] = data
+            noecho ? '' : data
+        else
+            i3bang_sections[$1]
+        end
+    }
+
+    # then expand !!<...> into separate lines
+    exrgx = /!!<([^>]*)>/
+    while config =~ exrgx
+        config.sub!(/^.*#{exrgx}.*$/) {|line|
+            expansions = line.scan(exrgx).map{|expansion|
+                group, values = expansion[0].split('!', 2)
+                if values == nil
+                    values = group
+                    group = "__default_group"
+                end
+                [group, values.gsub(/(\d+)\.\.(\d+)/) {
+                    [*$1.to_i..$2.to_i] * ?,
+                }.split(?,, -1)]
+            }
+            group = expansions[0][0]
+            # equalize length of values for same groups
+            maxlen = expansions.select{|g, _| g == group}.map(&:last).map(&:size).max
+            expansions.map! {|g, values|
+                g == group ?
+                    [g, (values * (maxlen * 1.0 / values.length).ceil)[0...maxlen]] :
+                    [g, values]
+            }
+            Array.new(expansions[0][1].length) { line.clone }.map {|l|
+                idx = -1
+                l.gsub(exrgx) {|m|
+                    idx += 1
+                    expansions[idx][0] == group ? expansions[idx][1].shift : m
+                }
+            }.join "\n"
+        }
+        expand_nobracket config if nobracket
+    end
+
+    # now replace all variables/math (!<...>) with their eval'd format
+    i3bang_vars = Hash.new {|_, k| k.is_a?(Symbol) ? nil : k }
+    config.gsub!(/(?<!!)!<([^>]*)>/) {
+        s = $1
+        # Now we i3bang_eval s
+        # http://en.wikipedia.org/wiki/Shunting-yard_algorithm
+        # we assume everything is left-associative for simplicity
+
+        # precedence and stacks setup
+        prec = Hash.new(-1).merge({
+            '=' => 0,
+            '+' => 1, '-' => 1,
+            '*' => 2, '/' => 2, '%' => 2,
+            '**' => 3
+        })
+        rpn = []
+        opstack = []
+
+        # tokenize input
+        tokens = s.gsub(/\s/, '').scan(/
+                        \w[\w\d]*     | # variable
+                        \d+(?:\.\d+)? | # number literal
+                        \*\*          | # multi-character operator
+                        .               # other operator
+                        /x)
+
+        # Shunting-yard
+        op = nil
+        tokens.each do |t|
+            case t[0]
+            when 'a'..'z', 'A'..'Z', '_'  # variable
+                rpn.push t.to_sym
+            when '0'..'9'  # number literal
+                rpn.push t.to_i
+            when '('  # open paren
+                opstack.push t
+            when ')'  # close paren
+                rpn.push op while (op = opstack.pop) != '('
+            else  # operator
+                rpn.push opstack.pop while prec[t] <= prec[opstack[-1]]
+                opstack.push t
+            end
+        end
+        rpn.push op while (op = opstack.pop)
+
+        # evaluate rpn
+        stack = []
+        rpn.each do |t|
+            case t
+            when Fixnum, Symbol
+                stack.push t
+            when '='
+                b, a = stack.pop, stack.pop
+                i3bang_vars[a] = i3bang_vars[b]
+            else
+                b, a = stack.pop, stack.pop
+                stack.push (case t
+                            when '+' then ->a, b { a + b }
+                            when '-' then ->a, b { a - b }
+                            when '*' then ->a, b { a * b }
+                            when '/' then ->a, b { a / b }
+                            when '%' then ->a, b { a % b }
+                            when '**' then ->a, b { a ** b }
+                           end)[i3bang_vars[a], i3bang_vars[b]]
+            end
+        end
+
+        i3bang_vars[stack[0]]
+    }
+
+    # cleanup: remove empty lines
+    config.gsub! /\n+/, "\n"
+    config.sub! /\A\n/, ''
+
+    config
+
 end
 
-# now replace all variables/math (!<...>) with their eval'd format
-i3bang_vars = Hash.new {|_, k| k.is_a?(Symbol) ? nil : k }
-config.gsub!(/(?<!!)!<([^>]*)>/) {
-    s = $1
-    # Now we i3bang_eval s
-    # http://en.wikipedia.org/wiki/Shunting-yard_algorithm
-    # we assume everything is left-associative for simplicity
+if __FILE__ == $0
+    fname = ARGV[0] || '_config'
+    INFILE = File.expand_path "~/.i3/#{fname}"
+    OUTFILE = File.expand_path '~/.i3/config'
 
-    # precedence and stacks setup
-    prec = Hash.new(-1).merge({
-        '=' => 0,
-        '+' => 1, '-' => 1,
-        '*' => 2, '/' => 2, '%' => 2,
-        '**' => 3
-    })
-    rpn = []
-    opstack = []
-
-    # tokenize input
-    tokens = s.gsub(/\s/, '').scan(/
-                    \w[\w\d]*     | # variable
-                    \d+(?:\.\d+)? | # number literal
-                    \*\*          | # multi-character operator
-                    .               # other operator
-                    /x)
-
-    # Shunting-yard
-    op = nil
-    tokens.each do |t|
-        case t[0]
-        when 'a'..'z', 'A'..'Z', '_'  # variable
-            rpn.push t.to_sym
-        when '0'..'9'  # number literal
-            rpn.push t.to_i
-        when '('  # open paren
-            opstack.push t
-        when ')'  # close paren
-            rpn.push op while (op = opstack.pop) != '('
-        else  # operator
-            rpn.push opstack.pop while prec[t] <= prec[opstack[-1]]
-            opstack.push t
-        end
-    end
-    rpn.push op while (op = opstack.pop)
-
-    # evaluate rpn
-    stack = []
-    rpn.each do |t|
-        case t
-        when Fixnum, Symbol
-            stack.push t
-        when '='
-            b, a = stack.pop, stack.pop
-            i3bang_vars[a] = i3bang_vars[b]
-        else
-            b, a = stack.pop, stack.pop
-            stack.push (case t
-                        when '+' then ->a, b { a + b }
-                        when '-' then ->a, b { a - b }
-                        when '*' then ->a, b { a * b }
-                        when '/' then ->a, b { a / b }
-                        when '%' then ->a, b { a % b }
-                        when '**' then ->a, b { a ** b }
-                       end)[i3bang_vars[a], i3bang_vars[b]]
-        end
-    end
-
-    i3bang_vars[stack[0]]
-}
-
-# cleanup: remove empty lines
-config.gsub! /\n+/, "\n"
-config.sub! /\A\n/, ''
-
-File.write OUTFILE, config
+    config = File.read INFILE
+    File.write(OUTFILE, i3bang(config, "
+    ##########
+    # Generated via i3bang (https://github.com/KeyboardFire/i3bang).
+    # Original file: #{fname}
+    ##########\n"))
+end
---
+++ test.rb
@@ -0,0 +1,104 @@
+#!/usr/bin/ruby
+
+require_relative 'i3bang.rb'
+require 'test/unit'
+
+module Test::Unit::Assertions
+    def assert_i3bang i3bang_in, i3bang_out, *a
+        assert_equal i3bang("#!nobracket\n#{i3bang_in}"), "#{i3bang_out}\n", *a
+    end
+end
+
+class TestI3bang < Test::Unit::TestCase
+    # a bit deceiving of a name; not all of these are strict identity
+    def test_identity
+        assert_i3bang 'test', 'test'
+        assert_i3bang "foo\nbar\nbaz", "foo\nbar\nbaz"
+        assert_i3bang "foo#!<1+>!!<a,b,c>\nbar", "foo\nbar"
+        assert_i3bang "foo  \nbar\t  \t ", "foo\nbar"
+    end
+
+    def test_variables
+        assert_i3bang '!<a=42>!a', '42'
+        assert_i3bang '!<foo = 1>!<bar = 2>!<foo>!<bar>', '12'
+        assert_i3bang "!n=1337\n!n", '1337'
+    end
+
+    def test_math_operations
+        assert_i3bang '!22+20', '42'
+        assert_i3bang '!6*9', '54'  # nice try, we're not in base 13
+        assert_i3bang '!200-13', '187'
+        assert_i3bang '!8/3', '2'  # int division
+        assert_i3bang '!8%3', '2'
+        assert_i3bang '!2**8', '256'
+    end
+
+    def test_math_precedence
+        assert_i3bang '!5+6*7', '47'
+        assert_i3bang '!(5+6)*7', '77'
+        assert_i3bang '!<a=1+2*3-4/5**6>!a', '7'
+    end
+
+    def test_basic_expansions
+        assert_i3bang 'b!!<a,o,i,u>t', "bat\nbot\nbit\nbut"
+        assert_i3bang 'e!!at,xterminate,xamine potatoes',
+            "eat potatoes\nexterminate potatoes\nexamine potatoes"
+        assert_i3bang 'pi!!e,ll,e,e,', "pie\npill\npie\npie\npi"
+    end
+
+    def test_range_expansions
+        assert_i3bang 'test !!8..11', "test 8\ntest 9\ntest 10\ntest 11"
+        assert_i3bang 'fizz!!1..4,buzz,6..7',
+            "fizz1\nfizz2\nfizz3\nfizz4\nfizzbuzz\nfizz6\nfizz7"
+        assert_i3bang '!!1,3..5,8,10..13', "1\n3\n4\n5\n8\n10\n11\n12\n13"
+    end
+
+    def test_dual_expansions
+        assert_i3bang 'bind !!1..9,0 workspace !!1..10',
+            "bind 1 workspace 1\n" + "bind 2 workspace 2\n" +
+            "bind 3 workspace 3\n" + "bind 4 workspace 4\n" +
+            "bind 5 workspace 5\n" + "bind 6 workspace 6\n" +
+            "bind 7 workspace 7\n" + "bind 8 workspace 8\n" +
+            "bind 9 workspace 9\n" + "bind 0 workspace 10"
+        assert_i3bang '!!<a,b,c,d,e>!!1,2,3', "a1\nb2\nc3\nd1\ne2"
+        assert_i3bang '!!<foo!a,b>!!foo!4,5,6,7', "a4\nb5\na6\nb7"
+        assert_i3bang '!!<1,2,3>!!<4,5>!!<6,7,8,9>!!<10>',
+            "14610\n25710\n34810\n15910"
+    end
+
+    def test_separate_expansions
+        assert_i3bang '!!<1!a,b,c>!!2!1,2', "a1\na2\nb1\nb2\nc1\nc2"
+        assert_i3bang '!!<foo,bar>!!x!1,2', "foo1\nfoo2\nbar1\nbar2"
+        assert_i3bang '!!<foo!i,j>!!3,4', "i3\ni4\nj3\nj4"
+    end
+
+    def test_multiple_expansion_groups
+        assert_i3bang '!!<A!a,b,c>!!A!1,2,3 !!<B!d,e>!!B!4,5',
+            "a1 d4\na1 e5\nb2 d4\nb2 e5\nc3 d4\nc3 e5"
+        assert_i3bang '!!<a,b,c>!!1,2 !!<x!x,y,z>!!x!3,4',
+            "a1 x3\na1 y4\na1 z3\nb2 x3\nb2 y4\nb2 z3\nc1 x3\nc1 y4\nc1 z3"
+        assert_i3bang '!!<4,7>!!<2,9,5>!!<x!,.>!!<0,12>',
+            "420\n42.0\n7912\n79.12\n450\n45.0"
+    end
+
+    def test_sections
+        assert_i3bang "!@<foo\nfoo\nbar\nbaz>\n!@foo",
+            "foo\nbar\nbaz\nfoo\nbar\nbaz"
+        assert_i3bang "!@<*a\nblah>\nfoo !@a baz", 'foo blah baz'
+    end
+
+    def test_line_continuations
+        assert_i3bang "foo\\\nbar", 'foobar'
+        assert_i3bang "foo  \\\n     bar", 'foo  bar'
+        assert_i3bang "foo\nbar\\\nbaz\\\nqux\nquux", "foo\nbarbazqux\nquux"
+        assert_i3bang "foo \\   #comment\nbar", 'foo bar'
+    end
+
+    def test_bang_parsing
+        assert_i3bang '!  1     +   1', '2'
+        assert_i3bang "!@<!!!\n!!!!!>!@<!!!>", '!!!!!!!!!!'
+        assert_i3bang '!1+1 + 2      ', '4'
+        assert_i3bang '   !1+1 + 2   ', '4'
+        assert_i3bang '_!1+1 + 2     ', '_2 + 2'
+    end
+end
